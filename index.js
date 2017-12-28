93fcdedda19c050b041e1edb536e79d0a4e4236c
mengyuhan <simonhmy@gmail.com>
2017-12-27 19:07:18 -0800

    config port

--- index.js
+++ index.js
@@ -42,8 +42,12 @@ app.get('*', (req, res) =>{
 
  
 
-
 // start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'production';
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.info(`Server running on http://localhost:${port} [${env}]`);
 });
